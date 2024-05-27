7c532bc6cbb64241ba1281a967e9fde628b23d01
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-05-28 00:00:52 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
