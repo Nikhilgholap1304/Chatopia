3f9a9cc5b5202ef3610892d941af974d0c0e0859
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-05-27 23:57:43 +0530

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,8 +12,9 @@ const Login = () => {
   };
   return (
     <>
-      <div className="w-full h-screen flex items-center justify-center">
-        <div className="w-fit h-fit flex items-center justify-center flex-col gap-5 bg-dark2 border border-brown-700 p-7 rounded-md shadow-lg">
+      <div className="w-full h-screen flex items-center justify-center relative">
+        <div className="bg-[url('./assets/bgImages/designBg1.png')] absolute top-0 left-0 bottom-0 w-full "></div>
+        <div className="w-fit h-fit flex items-center justify-center flex-col gap-5 bg-dark2 border border-brown-700 p-7 rounded-md shadow-lg scale-[0.95] xs:scale-100">
           <h1 className="font-roboto text-2xl mb-5">
             Welcome to <span className="font-adventPro text-3xl font-bold">Chatopia</span>
           </h1>
