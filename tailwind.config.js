6aac7bdb1d101dc039bf4d85087dc429a89e6f19
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-05-27 17:32:26 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: {}
+      fontFamily: {
+        'roboto': ['Roboto']
+      },
+      colors: {
+        color: {
+          1: "#3E3232",
+          2: "#503C3C",
+          3: "#7E6363",
+          4: "#A87C7C",
+          5: "#C29999",
+          6: "#E0B6B6",
+        }
+      }
     },
   },
   plugins: [],
