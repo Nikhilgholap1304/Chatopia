0dce2f312b195a34ff410dfa84983cf266a113c6
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-06-06 23:26:57 +0530

    update

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,24 +1,33 @@
-import { IconButton } from "@material-tailwind/react";
+import { Button, IconButton } from "@material-tailwind/react";
 import React, { useEffect, useState } from "react";
 import Avatar from "react-avatar";
-import { IoCallOutline, IoEllipsisVertical, IoSearch } from "react-icons/io5";
+import {
+  IoCallOutline,
+  IoEllipsisVertical,
+  IoSearch,
+  IoSend,
+} from "react-icons/io5";
 import Demo from "../assets/Demo.jpg";
-import { FaArrowLeft, FaRegUser, FaUserGroup } from "react-icons/fa6";
+import { FaArrowLeft } from "react-icons/fa6";
 import { useMediaQuery } from "react-responsive";
 import { motion } from "framer-motion";
-import { BsCameraVideo } from "react-icons/bs";
-import { MdDeleteOutline } from "react-icons/md";
+import { BsCameraVideo, BsEmojiSmile } from "react-icons/bs";
 import { FiLock } from "react-icons/fi";
 import { AiOutlineDelete } from "react-icons/ai";
+import { GrAttachment } from "react-icons/gr";
 
 const Chat = ({ setSideBarOpen, sideBarOpen }) => {
   const [sideMenuOpen, setSideMenuOpen] = useState(false);
+  const [isMsg, setIsMsg] = useState("");
   const Max1080 = useMediaQuery({
     query: "(max-width: 1080px)",
   });
+  const handleIsMsgChange = (e) => {
+    setIsMsg(e.target.value);
+  };
   return (
     <div
-      className={`w-full h-full flex-2 2xs:translate-x-[100vw] 2xs:flex-[0] lg:!translate-x-0 lg:!flex-1 lg:!w-full flex relative transition-all ${
+      className={`w-full h-full flex-2 2xs:translate-x-[100vw] 2xs:flex-[0] lg:!translate-x-0 lg:!flex-1 lg:!w-full flex flex-col relative transition-all ${
         !sideBarOpen && "2xs:!translate-x-0 2xs:flex-[1]"
       }`}
       style={{
@@ -26,8 +35,15 @@ const Chat = ({ setSideBarOpen, sideBarOpen }) => {
           "linear-gradient(to right bottom, #322200, #372701, #3d2b01, #423001, #473501, #4c3901, #523e01, #574201, #5e4701, #654b01, #6c5000, #735500",
       }}
     >
-      <div className="bg-[url('./assets\bgImages\designBg1.png')] absolute inset-0" />
-      <div className="absolute top-0 left-0 right-0 bg-graysurface w-full 2xs:px-2 sm:px-4 md:px-5 py-2 shadow flex justify-between items-center gap-3 cursor-pointer">
+      <div className="bg-[url('./assets\bgImages\designBg1.png')] absolute inset-0 -z-[1]" />
+      <div
+        className={`relative bg-graysurface 2xs:px-2 sm:px-4 md:px-5 py-2 shadow ${
+          !sideBarOpen
+            ? "w-full visible !flex"
+            : "2xs:w-0 2xs:hidden 2xs:invisible"
+        } 
+      lg:!w-full lg:!flex lg:!visible justify-between items-center gap-3 cursor-pointer`}
+      >
         <div className="flex gap-2 items-center justify-center">
           <IconButton
             className={`hover:bg-graylightsecondarytextcolor xs:!p-5 2xs:p-2 ${
@@ -105,27 +121,74 @@ const Chat = ({ setSideBarOpen, sideBarOpen }) => {
                   </div>
                   <div className="flex gap-3 items-center hover:bg-graylightsecondarytextcolor px-5 py-2 rounded transition-all active:scale-[0.95]">
                     <BsCameraVideo className="size-[1.2rem]" />
-                    <span className="font-medium text-sm">
-                      Video Call
-                    </span>
+                    <span className="font-medium text-sm">Video Call</span>
                   </div>
                   <div className="flex gap-3 items-center hover:bg-graylightsecondarytextcolor px-5 py-2 rounded transition-all active:scale-[0.95]">
                     <FiLock className="size-[1.2rem]" />
-                    <span className="font-medium text-sm">
-                      Block User
-                    </span>
+                    <span className="font-medium text-sm">Block User</span>
                   </div>
                   <div className="flex gap-3 items-center hover:bg-graylightsecondarytextcolor px-5 py-2 rounded transition-all active:scale-[0.95] text-red-300">
                     <AiOutlineDelete className="size-[1.4rem]" />
-                    <span className="font-medium text-sm">
-                      Delete Chat
-                    </span>
+                    <span className="font-medium text-sm">Delete Chat</span>
                   </div>
                 </motion.div>
               </div>
             </div>
           </div>
         </div>
+      </div>
+      <div className="flex-1"></div>
+      <div
+        className={`relative ${
+          !sideBarOpen ? "visible w-full flex" : "invisible w-0 hidden"
+        } lg:visible lg:w-full lg:flex transition-all justify-center items-center gap-2 py-[0.5rem]`}
+      >
+        <div className="max-w-[80%] flex-1 bg-graysurface rounded-lg rounded-br-none items-center">
+          <div className="flex flex-1 shadow-lg relative items-end">
+            <div className="flex items-center p-2">
+              <Button className="xs:p-2 p-1 rounded-full hover:bg-graylightsecondarytextcolor transition-all cursor-pointer active:text-brown-200">
+                <BsEmojiSmile className="text-xl text-graysecondarytextcolor" />
+              </Button>
+            </div>
+            <div className="flex-[1] relative flex items-center">
+              <textarea
+                type="text"
+                className=" bg-transparent text-sm caret-brown-200 border-none outline-none relative z-[1] w-full py-2 flex items-center"
+                onChange={(e)=>handleIsMsgChange(e)}
+                value={isMsg}
+                wrap="soft"
+              />
+              <motion.span
+                className="absolute left-0 origin-right text-graysecondarytextcolor xs:text-base text-sm"
+                animate={{
+                  x: !isMsg ? 0 : 15,
+                  opacity: !isMsg ? 1 : 0,
+                }}
+                transition={{
+                  duration: 0.2
+                }}
+              >
+                Message
+              </motion.span>
+            </div>
+            <div className="flex items-center p-2">
+              <Button className="xs:p-2 p-1 rounded-full hover:bg-graylightsecondarytextcolor transition-all active:text-brown-200">
+                <GrAttachment className="text-xl cursor-pointer text-graysecondarytextcolor" />
+              </Button>
+            </div>
+            <div
+              style={{
+                aspectRatio: 1,
+                clipPath: "polygon(0 0,100% 100%,0 100%)",
+                transform: "translateX",
+              }}
+              className="absolute w-2 h-3 left-[99.5%] bottom-0 bg-graysurface"
+            ></div>{" "}
+          </div>
+        </div>
+        <Button className="xs:p-[1rem] p-[0.7rem] bg-brown-400 rounded-full">
+          <IoSend className="xs:size-[1.4rem] size-[1.3rem] translate-x-[1px]" />
+        </Button>
       </div>
     </div>
   );
