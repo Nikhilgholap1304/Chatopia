2103b312b31497d4dba953d5f50eab0582ff4b5b
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-06-11 20:41:00 +0530

    update

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -4,8 +4,20 @@ import "./style/style.scss";
 import { Avatar, Button } from "@material-tailwind/react";
 import Ripples from "react-ripples";
 import { motion } from "framer-motion";
+import { useUserStore } from "../lib/userStore";
 
 const ChatList = ({ searchActive, setSideBarOpen, sideBarOpen }) => {
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useUserStore();
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "users", "SF"), (doc) => {
+      console.log("Current data: ", doc.data());
+    });
+    return () => {
+      unSub();
+    };
+  }, [[currentUser.id]]);
+
   return (
     <>
       <motion.section
@@ -20,11 +32,11 @@ const ChatList = ({ searchActive, setSideBarOpen, sideBarOpen }) => {
       >
         <div
           className={`flex hover:bg-graylightsecondarytextcolor cursor-pointer relative `}
-          >
+        >
           <Ripples
             className="absolute w-full h-full flex p-2 gap-2 items-center"
             during={1200}
-            onClick={()=>setSideBarOpen(false)}
+            onClick={() => setSideBarOpen(false)}
           >
             <div className=" rounded-full size-[3rem] min-w-[3rem]">
               {/* <div className="w-full h-full bg-gray-800 rounded-full animate-pulse"/> */}
@@ -35,9 +47,13 @@ const ChatList = ({ searchActive, setSideBarOpen, sideBarOpen }) => {
             </div>
             <div className="flex flex-col justify-center w-full gap-1">
               <div className="flex justify-between">
-                <h5 className="xs:leading-5 xs:text-base leading-4 text-sm">Arjun Pandey</h5>
+                <h5 className="xs:leading-5 xs:text-base leading-4 text-sm">
+                  Arjun Pandey
+                </h5>
                 {/* <div className="w-[10rem] h-4 bg-gray-800 animate-pulse rounded"/> */}
-                <p className="xs:text-xs text-[0.7rem] leading-3 text-graysecondarytextcolor">15:35</p>
+                <p className="xs:text-xs text-[0.7rem] leading-3 text-graysecondarytextcolor">
+                  15:35
+                </p>
                 {/* <div className="w-[2rem] h-3 bg-gray-800 animate-pulse rounded"/> */}
               </div>
               <div className="flex justify-between items-center gap-2">
