7c8385a94d6b957efe5af4db7ff32fb8ed2520dd
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-06-12 18:56:37 +0530

    update

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -5,25 +5,31 @@ import { Avatar, Button } from "@material-tailwind/react";
 import Ripples from "react-ripples";
 import { motion } from "framer-motion";
 import { useUserStore } from "../lib/userStore";
-import { doc, onSnapshot } from "firebase/firestore";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
 import { db } from "../lib/firebase";
 import { useAuth } from "../context/AuthContext";
 
-
 const ChatList = ({ searchActive, setSideBarOpen, sideBarOpen }) => {
-  const {currentUid} = useAuth();
+  const { currentUid } = useAuth();
   const [chats, setChats] = useState([]);
   const { currentUser } = useUserStore();
-  console.log(currentUser)
+  console.log(currentUser);
   useEffect(() => {
-    const unSub = onSnapshot(doc(db, "users",currentUser.id), (doc) => {
-      console.log("Current data: ", doc.data());
+    const unSub = onSnapshot(doc(db, "users", currentUser.id), (res) => {
+      // console.log("Current data: ", doc.data());
+      const items = res.data().chats;
+
+      const promisses = items.map(async (item) => {
+        const userDocRef = doc(db, "users", item.receiverId);
+        const userDocSnap = await getDoc(userDocRef);
+        const user = userDocSnap.data();
+      });
     });
     return () => {
       unSub();
     };
   }, [currentUser.id]);
-  console.log(chats)
+  // console.log(chats);
 
   return (
     <>
@@ -37,49 +43,52 @@ const ChatList = ({ searchActive, setSideBarOpen, sideBarOpen }) => {
           duration: 0.1,
         }}
       >
-        <div
-          className={`flex hover:bg-graylightsecondarytextcolor cursor-pointer relative `}
-        >
-          <Ripples
-            className="absolute w-full h-full flex p-2 gap-2 items-center"
-            during={1200}
-            onClick={() => setSideBarOpen(false)}
+        {chats.map((chat) => (
+          <div
+            className={`flex hover:bg-graylightsecondarytextcolor cursor-pointer relative `}
+            key={chat.chatId}
           >
-            <div className=" rounded-full size-[3rem] min-w-[3rem]">
-              {/* <div className="w-full h-full bg-gray-800 rounded-full animate-pulse"/> */}
-              <Avatar
-                src={Demo}
-                className="w-full h-full border-brown-200 border-[1px]"
-              />
-            </div>
-            <div className="flex flex-col justify-center w-full gap-1">
-              <div className="flex justify-between">
-                <h5 className="xs:leading-5 xs:text-base leading-4 text-sm">
-                  Arjun Pandey
-                </h5>
-                {/* <div className="w-[10rem] h-4 bg-gray-800 animate-pulse rounded"/> */}
-                <p className="xs:text-xs text-[0.7rem] leading-3 text-graysecondarytextcolor">
-                  15:35
-                </p>
-                {/* <div className="w-[2rem] h-3 bg-gray-800 animate-pulse rounded"/> */}
+            <Ripples
+              className="absolute w-full h-full flex p-2 gap-2 items-center"
+              during={1200}
+              onClick={() => setSideBarOpen(false)}
+            >
+              <div className=" rounded-full size-[3rem] min-w-[3rem]">
+                {/* <div className="w-full h-full bg-gray-800 rounded-full animate-pulse"/> */}
+                <Avatar
+                  src={Demo}
+                  className="w-full h-full border-brown-200 border-[1px]"
+                />
               </div>
-              <div className="flex justify-between items-center gap-2">
-                <p
-                  className="text-nowrap flex-[10rem]
+              <div className="flex flex-col justify-center w-full gap-1">
+                <div className="flex justify-between">
+                  <h5 className="xs:leading-5 xs:text-base leading-4 text-sm">
+                    Arjun Pandey
+                  </h5>
+                  {/* <div className="w-[10rem] h-4 bg-gray-800 animate-pulse rounded"/> */}
+                  <p className="xs:text-xs text-[0.7rem] leading-3 text-graysecondarytextcolor">
+                    15:35
+                  </p>
+                  {/* <div className="w-[2rem] h-3 bg-gray-800 animate-pulse rounded"/> */}
+                </div>
+                <div className="flex justify-between items-center gap-2">
+                  <p
+                    className="text-nowrap flex-[10rem]
                   w-[10rem] whitespace-nowrap
               text-ellipsis overflow-hidden xs:text-base text-sm text-graysecondarytextcolor"
-                >
-                  This message couldnt be displayed and all
-                </p>
-                {/* <div className="w-[15rem] h-4 bg-gray-800 animate-pulse rounded"/> */}
-                <span className="bg-brown-500 xs:size-6 2xs:size-5 grid place-content-center rounded-full leading-none text-xs">
-                  2
-                </span>
-                {/* <div className="w-4 h-4 bg-gray-800 animate-pulse rounded"/> */}
+                  >
+                    {chat.lastMessage}
+                  </p>
+                  {/* <div className="w-[15rem] h-4 bg-gray-800 animate-pulse rounded"/> */}
+                  <span className="bg-brown-500 xs:size-6 2xs:size-5 grid place-content-center rounded-full leading-none text-xs">
+                    2
+                  </span>
+                  {/* <div className="w-4 h-4 bg-gray-800 animate-pulse rounded"/> */}
+                </div>
               </div>
-            </div>
-          </Ripples>
-        </div>
+            </Ripples>
+          </div>
+        ))}
       </motion.section>
     </>
   );
